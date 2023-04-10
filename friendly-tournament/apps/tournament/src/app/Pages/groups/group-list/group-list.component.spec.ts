932b81e164c7b75200b207a91b1ff63f478a62ba
Tony <tnd.lamquang@student.avans.nl>
2023-04-10 20:19:14 +0200

    front-end tests

--- friendly-tournament/apps/tournament/src/app/Pages/groups/group-list/group-list.component.spec.ts
+++ friendly-tournament/apps/tournament/src/app/Pages/groups/group-list/group-list.component.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GroupEditComponent } from './group-edit.component';
+
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GroupService } from '../group.service';
 import { IGroup } from '@friendly-tournament/data/models';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { FormBuilder, FormsModule } from '@angular/forms';
 import { of } from 'rxjs';
+import { GroupListComponent } from './group-list.component';
 
-describe('GroupEditComponent', () => {
-    let component: GroupEditComponent;
-    let fixture: ComponentFixture<GroupEditComponent>;
+describe('GroupListComponent', () => {
+    let component: GroupListComponent;
+    let fixture: ComponentFixture<GroupListComponent>;
 
     const dummyGroup: IGroup = {
         _id: '1',
@@ -44,7 +45,7 @@ describe('GroupEditComponent', () => {
 
     const groupServiceMock = {
         getGroup: jest.fn().mockImplementation(() => of(dummyGroup)),
-        getGroups: jest.fn().mockImplementation(() => of(groupList)),
+        getGroups: jest.fn().mockImplementation(),
         createGroup: jest.fn(),
         updateGroup: jest.fn(),
         leaveGroup: jest.fn(),
@@ -52,8 +53,8 @@ describe('GroupEditComponent', () => {
     }
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [GroupEditComponent],
-            imports: [HttpClientTestingModule, FormsModule],
+            declarations: [GroupListComponent],
+            imports: [HttpClientTestingModule, FormsModule, RouterModule],
             providers: [
                 {
                     provide: GroupService, useValue: groupServiceMock
@@ -67,12 +68,12 @@ describe('GroupEditComponent', () => {
         }).compileComponents();
 
         
-        fixture = TestBed.createComponent(GroupEditComponent);
+        fixture = TestBed.createComponent(GroupListComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    describe('create', ()=>{
+    describe('list', ()=>{
         beforeEach(() => {
             activatedRouteMock.snapshot.paramMap.get = jest.fn().mockImplementation(() => { return null });
         });
@@ -81,45 +82,9 @@ describe('GroupEditComponent', () => {
             expect(component).toBeTruthy();
         });
 
-        it('should call getGroup', () => {
-            expect(groupServiceMock.getGroup).toHaveBeenCalledTimes(0);
+        it('should call getGroups', () => {
+            expect(groupServiceMock.getGroups).toHaveBeenCalledTimes(2);
         })
-
-        it('should give a empty form when no id is given', () => {
-            const group = {
-            } as IGroup;
-
-            expect(component.group)
-            expect(component.group).toEqual(group);
-        });
-        
-    })
-
-    describe('edit', ()=>{
-        beforeEach(() => {
-            activatedRouteMock.snapshot.paramMap.get = jest.fn().mockImplementation(() => { return '1' });
-        });
-
-        it('should create', () => {
-            expect(component).toBeTruthy();
-        });
-
-        it('should call getGroup', () => {
-            expect(groupServiceMock.getGroup).toHaveBeenCalled();
-        })
-
-        it('should give a form with the group when id is given', () => {
-            const group = {
-                _id: '1',
-                Name: 'Test',
-                Description: 'Test',
-                CreatedDate: new Date('2021-01-01T00:00:00.000Z'),
-                Entries: [],
-                Invites: [],
-                Users: [],
-            } as IGroup;
-
-            expect(component.group).toEqual(group);
-        });
     });
+
 });
